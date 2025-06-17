27fa20b8dd747116e0accdb551205f0880964b49
vzhyhunou <vicazh@tut.by>
2025-06-17 18:54:52 +0300

    move admin logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 
-import Context, { useContextProvider } from './Context';
-import Core from './Core';
-import Parser from './admin/Parser';
+import Context from './Context';
+import Admin from './admin/App';
 import Component from './cms/Component';
 
-const SCHEMA = 'schema';
-
-const App = () => {
-  const {
-    dataProvider: { getList }
-  } = useContextProvider();
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    getList(SCHEMA, {
-      pagination: { page: 1, perPage: Number.MAX_SAFE_INTEGER },
-      sort: { field: 'id', order: 'ASC' }
-    }).then(({ data }) => setData(data));
-  }, [getList]);
-
-  if (!data) {
-    return;
-  }
-
-  const resources = Object.fromEntries(
-    data
-      .map(({ id, list, edit }) => ({
-        id,
-        list: <Parser jsx={list} />,
-        edit: <Parser jsx={edit} />
-      }))
-      .map(({ id, list, edit }) => [id, { list, edit }])
-  );
-
-  return (
-    <Core {...{ resources }}>
+export default ({ config }) => (
+  <Context {...config}>
+    <Admin>
       <Route
         path=""
         element={<Component resource="page" name="one" id="home" titled />}
@@ -50,12 +21,6 @@ const App = () => {
         path="*"
         element={<Component resource="page" name="one" id="none" titled />}
       />
-    </Core>
-  );
-};
-
-export default ({ config }) => (
-  <Context {...config}>
-    <App />
+    </Admin>
   </Context>
 );
--- client/src/admin/App.jsx
+++ client/src/admin/App.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
-import { Admin } from 'react-admin';
+import React, { useState, useEffect } from 'react';
+import { Admin, CustomRoutes, Resource, Layout } from 'react-admin';
+import { Route } from 'react-router-dom';
 
 import { useContextProvider } from '../Context';
+import Parser from './Parser';
 
-export default ({ children, ...rest }) => {
+const SCHEMA = 'schema';
+
+export default ({ children }) => {
   const { dataProvider } = useContextProvider();
+  const { getList } = dataProvider;
+  const [data, setData] = useState();
 
-  return <Admin {...{ dataProvider, ...rest }}>{children}</Admin>;
+  useEffect(() => {
+    getList(SCHEMA, {
+      pagination: { page: 1, perPage: Number.MAX_SAFE_INTEGER },
+      sort: { field: 'id', order: 'ASC' }
+    }).then(({ data }) => setData(data));
+  }, [getList]);
+
+  if (!data) {
+    return;
+  }
+
+  const resources = Object.fromEntries(
+    data
+      .map(({ id, list, edit }) => ({
+        id,
+        list: <Parser jsx={list} />,
+        edit: <Parser jsx={edit} />
+      }))
+      .map(({ id, list, edit }) => [id, { list, edit }])
+  );
+
+  return (
+    <Admin basename="/admin" {...{ dataProvider }}>
+      {() => {
+        const access = Object.entries(resources).map(([k, v]) => [
+          k,
+          <Resource key={k} name={k} {...v} />
+        ]);
+        return (
+          <>
+            <CustomRoutes noLayout>
+              <Route path="admin">
+                <Route path="" element={<Layout />} />
+                {access.map(([k, v]) => (
+                  <Route
+                    key={k}
+                    path={`${k}/*`}
+                    element={<Layout>{v}</Layout>}
+                  />
+                ))}
+              </Route>
+              {children}
+            </CustomRoutes>
+            {access.map(([k, v]) => v)}
+          </>
+        );
+      }}
+    </Admin>
+  );
 };
