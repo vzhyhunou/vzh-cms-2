a676c2e70a38eb5d050fbb4d9fd0adc56c0bfab5
vzhyhunou <vicazh@tut.by>
2025-08-06 21:06:35 +0300

    static module

--- src/app.module.js
+++ src/app.module.js
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { ConfigModule } from './config/config.module';
 import { ResourcesModule } from './resources/resources.module';
+import { StaticModule } from './static/static.module';
 
 @Module({
-  imports: [ConfigModule, ResourcesModule]
+  imports: [ConfigModule, ResourcesModule, StaticModule]
 })
 export class AppModule {}
