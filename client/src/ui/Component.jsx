7a8bd5ecd641615673b669628b8237c91ec5fb9e
vzhyhunou <vicazh@tut.by>
2025-08-13 19:52:29 +0300

    rename component bindings

--- client/src/ui/Component.jsx
+++ client/src/ui/Component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Parser from './Parser';
 import useGetComponent from '../provider/data/useGetComponent';
 
-const Component = ({ resource, name, ...params }) => {
+const Component = ({ resource, name, ...props }) => {
   const { data, isLoading, error } = useGetComponent(resource, { name });
 
   if (isLoading) {
@@ -14,7 +14,7 @@ const Component = ({ resource, name, ...params }) => {
     return <Component resource="page" name="one" id="none" />;
   }
 
-  return <Parser jsx={data} bindings={{ params }} />;
+  return <Parser jsx={data} bindings={{ props }} />;
 };
 
 export default Component;
