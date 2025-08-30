00caab5c1302bb5040e906d54cd3aa6697d0a553
vzhyhunou <vicazh@tut.by>
2025-08-30 12:35:38 +0300

    move routes to components from settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,66 @@
-import React, { createElement } from 'react';
-import { Route, useParams } from 'react-router-dom';
+import React from 'react';
+import { Admin, CustomRoutes, Resource, Layout } from 'react-admin';
+import { Route } from 'react-router-dom';
 
-import Context from './Context';
-import Admin from './Admin';
-import Component from './ui/Component';
+import Context, { useContextProvider } from './Context';
+import { AdminParser } from './ui/Parser';
+import addUploadFeature from './provider/data/upload';
+import parse from './ui/parse';
 
-const Element = (props) => {
-  const { '*': extra, ...p } = useParams();
-  return <Component {...{ ...p, ...props }} />;
-};
+const App = () => {
+  const contextProvider = useContextProvider();
+  const dataProvider = addUploadFeature(contextProvider);
+  const { authProvider } = contextProvider;
+  const { getResources, getComponent } = dataProvider;
 
-const parse = ({ element, children = [], ...rest }) =>
-  createElement(
-    Route,
-    {
-      element: <Element {...element} />,
-      ...rest
-    },
-    ...children.map(parse)
+  return (
+    <Admin basename="/admin" {...{ dataProvider, authProvider }}>
+      {() =>
+        Promise.all([
+          getResources({}),
+          getComponent('schema', { name: 'Routes' })
+        ]).then(([{ data }, { data: routes }]) => {
+          const resources = data
+            .filter(({ list }) => list)
+            .map(({ id, list, create, edit }) => ({
+              id,
+              ...Object.fromEntries(
+                Object.entries({ list, create, edit }).map(([k, v]) => [
+                  k,
+                  <AdminParser code={v} />
+                ])
+              )
+            }))
+            .map(({ id, ...rest }) => ({
+              id,
+              resource: <Resource key={id} name={id} {...rest} />
+            }));
+          return (
+            <>
+              <CustomRoutes noLayout>
+                <Route path="admin">
+                  <Route path="" element={<Layout />} />
+                  {resources.map(({ id, resource }) => (
+                    <Route
+                      key={id}
+                      path={`${id}/*`}
+                      element={<Layout>{resource}</Layout>}
+                    />
+                  ))}
+                </Route>
+                {parse(routes)}
+              </CustomRoutes>
+              {resources.map(({ resource }) => resource)}
+            </>
+          );
+        })
+      }
+    </Admin>
   );
+};
 
 export default ({ config }) => (
   <Context {...config}>
-    <Admin>{parse}</Admin>
+    <App />
   </Context>
 );
