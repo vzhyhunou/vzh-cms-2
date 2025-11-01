13b2e31d656b5a2f285ddaad6cfdb9411e5df5f1
vzhyhunou <vicazh@tut.by>
2025-11-01 11:32:21 +0300

    upd auth

--- client/src/provider/back/auth.js
+++ client/src/provider/back/auth.js
@@ -2,65 +2,64 @@ import decodeJwt from 'jwt-decode';
 
 const TOKEN = 'token';
 
-const setItem = (value) => localStorage.setItem(TOKEN, value);
-const getItem = () => localStorage.getItem(TOKEN);
-const removeItem = () => localStorage.removeItem(TOKEN);
-const getClaims = () => {
-  const token = getItem();
-  if (!token) {
-    return {};
-  }
-  const claims = decodeJwt(token);
-  const { exp } = claims;
-  if (exp > Date.now() / 1000) {
-    return claims;
-  }
-  removeItem();
-  return {};
+export default () => {
+  const auth = {
+    async login({ username, password }) {
+      const credentials = btoa(`${username}:${password}`);
+      const headers = { Authorization: `Basic ${credentials}` };
+      const response = await fetch('/auth', { headers });
+      const { status, statusText } = response;
+      if (status < 200 || status >= 300) {
+        throw new Error(statusText);
+      }
+      await auth.setToken(await response.text());
+      return { redirectTo: '/' };
+    },
+    async logout() {
+      await auth.removeToken();
+      return '/';
+    },
+    async checkError({ status }) {
+      if (status === 401 || status === 403) {
+        await auth.removeToken();
+        throw new Error();
+      }
+    },
+    async checkAuth() {
+      if (!(await auth.getToken())) {
+        throw new Error();
+      }
+    },
+    async getClaims() {
+      const token = await auth.getToken();
+      if (!token) {
+        return {};
+      }
+      const claims = decodeJwt(token);
+      const { exp } = claims;
+      if (exp > Date.now() / 1000) {
+        return claims;
+      }
+      await auth.removeToken();
+      return {};
+    },
+    async getPermissions() {
+      const { roles } = await auth.getClaims();
+      return roles;
+    },
+    async getIdentity() {
+      const { sub } = await auth.getClaims();
+      return { id: sub };
+    },
+    async setToken(value) {
+      localStorage.setItem(TOKEN, value);
+    },
+    async getToken() {
+      return localStorage.getItem(TOKEN);
+    },
+    async removeToken() {
+      localStorage.removeItem(TOKEN);
+    }
+  };
+  return auth;
 };
-
-export default () => ({
-  async login({ username, password }) {
-    const credentials = btoa(`${username}:${password}`);
-    const auth = { Authorization: `Basic ${credentials}` };
-    const response = await fetch('/auth', { headers: auth });
-    const { status, statusText } = response;
-    if (status < 200 || status >= 300) {
-      throw new Error(statusText);
-    }
-    setItem(await response.text());
-    return { redirectTo: '/' };
-  },
-  async logout() {
-    removeItem();
-    return '/';
-  },
-  async checkError({ status }) {
-    if (status === 401 || status === 403) {
-      removeItem();
-      throw new Error();
-    }
-  },
-  async checkAuth() {
-    if (!getItem()) {
-      throw new Error();
-    }
-  },
-  async getPermissions() {
-    const { roles } = getClaims();
-    return roles;
-  },
-  async getIdentity() {
-    const { sub } = getClaims();
-    return { id: sub };
-  },
-  async setToken(value) {
-    setItem(value);
-  },
-  async getToken() {
-    return getItem();
-  },
-  async removeToken() {
-    removeItem();
-  }
-});
