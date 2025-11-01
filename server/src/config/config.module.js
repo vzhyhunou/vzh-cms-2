f038dafdf561ff065ec0e40027bfa24f89f986d7
vzhyhunou <vicazh@tut.by>
2025-11-02 00:30:34 +0300

    upd config location

--- server/src/config/config.module.js
+++ server/src/config/config.module.js
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import merge from 'lodash/merge';
+import path from 'path';
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
       load: [
         Promise.all([
-          import('../../config').catch(() => {}),
-          import(`../../${process.env.NODE_ENV}.config`).catch(() => {})
+          import(path.join(process.cwd(), 'config')).catch(() => {}),
+          import(
+            path.join(process.cwd(), `${process.env.NODE_ENV}.config`)
+          ).catch(() => {})
         ]).then(
           (args) => () =>
             args
