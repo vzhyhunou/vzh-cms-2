de6a478668d9a1b7c973336422689ac486f66566
vzhyhunou <vicazh@tut.by>
2025-11-27 22:50:27 +0300

    bundle analyzer

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import { analyzer } from 'vite-bundle-analyzer';
 
 const config = ({
     REACT_APP_SRC,
     REACT_APP_BASE,
     BUILD_PATH
 }) => ({
-    plugins: [react(), eslint()],
+    plugins: [react(), eslint(), analyzer()],
     server: {
         port: 3010
     },
